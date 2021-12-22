bc11bd55fd85e9b4f39dbbf5e318f62c5de79876
SantiBass <sibarraryan@gmail.com>
2021-12-22 12:36:18 -0800

    working on the query data file

--- db/query_db.sql
+++ db/query_db.sql
@@ -1,28 +1,4 @@
+SELECT * FROM departments;
 
- DATABASE IF EXISTS employees_db;
-CREATE employees_db;
-
-USE employees_db;
-
-CREATE TABLE  departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    
-);
-
-CREATE TABLE  roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title_of_role VARCHAR(30), NOT NULL,
-    salary DECIMAL(13,4),
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
-
-);
-CREATE TABLE  employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT ,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-);
+-- selecting data from from all roles.
+SELECT 
