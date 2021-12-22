133fff3c05a9b68e25fa0291ef0ff01778cd2c6f
SantiBass <sibarraryan@gmail.com>
2021-12-21 17:16:08 -0800

    working on the add a role feature

--- server.js
+++ server.js
@@ -1,90 +1,98 @@
 const inquirer = require('inquirer');
 const { add } = require('lodash');
-const PORT  = process.env.PORT ||  3005;
+const PORT = process.env.PORT || 3005;
 const mysql = require('mysql');
-require ("console.table");
+require("console.table");
 
-function init(){
-inquirer.prompt({
-type: "list",
-name : "ChoosingOptions",
-message: "===============================================\n  ****  Welcome to the Employee Traker app.  ****\n  ****  What would you like to do today?     **** \n  =============================================== \n",
-
-choices: ['View all departments','View all roles', 'View all employees', 'Add departments', 'Add a role', 'Add an employee', 'Update  employee role',  'quit']
-}).then(function(selectedAnswer){
-    if(selectedAnswer.ChoosingOptions === "View all departments"){
-        inquirer.prompt({
-            // add table HERE for departments BY NAME AND ID
-            type: 'list',
-            name: 'ChoosingOptions',
-            message: "Here are all departments \n  Press enter to go to the main menu plase",
-            choices:['Go to the main menu']
-            
-        }).then(function(selectedAnswer){
-            if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
-               init();
-
-                
-            }
-        })
-
-     } else if(selectedAnswer.ChoosingOptions === "View all roles"){
-        inquirer.prompt({
-        // add table HERE for all roles presented with the job title, role id, the department that role belongs to, and the salary for that role
-        type: 'list',
-        name: 'ChoosingOptions',
-        message: "Here are all the roles \n  Press enter to go to the main menu plase",
-        choices:['Go to the main menu']
-    }).then(function(selectedAnswer){
-        if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
-           init();
-
-            
-        };
-      });
-
- } else if(selectedAnswer.ChoosingOptions === "View all employees"){
+function init() {
     inquirer.prompt({
-    // add table HERE for all employees presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    type: 'list',
-    name: 'ChoosingOptions',
-    message: "Here are all the employees \n  Press enter to go to the main menu plase",
-    choices:['Go to the main menu']
-}).then(function(selectedAnswer){
-    if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
-       init();
+        type: "list",
+        name: "ChoosingOptions",
+        message: "===============================================\n  ****  Welcome to the Employee Traker app.  ****\n  ****  What would you like to do today?     **** \n  =============================================== \n",
 
-        
-    };
-   });
-  }else if(selectedAnswer.ChoosingOptions === "Add departments"){
-    inquirer.prompt([
-        {
-        type: 'input',
-        name: 'nameOfDepartment',
-        message: "Give a name to the department you want to add",
-        
-        },
-        {
-        type: 'input',
-        name: 'idOfDepartment',
-        message: "Give an ID to the department you want to add",
-        }
-        // add department here
-    ]).then(function(selectedAnswer){
-        if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
-            init();
-     
-             
-         };
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add departments', 'Add a role', 'Add an employee', 'Update  employee role', 'quit']
+    }).then(function (selectedAnswer) {
+        if (selectedAnswer.ChoosingOptions === "View all departments") {
+            inquirer.prompt({
+                // add table HERE for departments BY NAME AND ID
+                type: 'list',
+                name: 'ChoosingOptions',
+                message: "Here are all departments \n  Press enter to go to the main menu plase",
+                choices: ['Go to the main menu']
+
+            }).then(function (selectedAnswer) {
+                if (selectedAnswer.ChoosingOptions === "Go to the main menu") {
+                    init();
+
+
+                }
+            })
+
+        } else if (selectedAnswer.ChoosingOptions === "View all roles") {
+            inquirer.prompt({
+                // add table HERE for all roles presented with the job title, role id, the department that role belongs to, and the salary for that role
+                type: 'list',
+                name: 'ChoosingOptions',
+                message: "Here are all the roles \n  Press enter to go to the main menu plase",
+                choices: ['Go to the main menu']
+            }).then(function (selectedAnswer) {
+                if (selectedAnswer.ChoosingOptions === "Go to the main menu") {
+
+                    init();
+
+
+                };
+            });
+
+        } else if (selectedAnswer.ChoosingOptions === "View all employees") {
+            inquirer.prompt({
+                // add table HERE for all employees presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+                type: 'list',
+                name: 'ChoosingOptions',
+                message: "Here are all the employees \n  Press enter to go to the main menu plase",
+                choices: ['Go to the main menu']
+            }).then(function (selectedAnswer) {
+                if (selectedAnswer.ChoosingOptions === "Go to the main menu") {
+                    init();
+
+
+                };
+            });
+        } else if (selectedAnswer.ChoosingOptions === "Add departments") {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'nameOfDepartment',
+                    message: "Give a name to the department you want to add",
+
+                },
+                {
+                    type: 'input',
+                    name: 'idOfDepartment',
+                    message: "Give an ID to the department you want to add",
+                },
+                {
+                    type: 'list',
+                    name: 'ChoosingOptions',
+                    message: "You added a deparment successfuly \n  Press enter to go to the main menu plase",
+                    choices: ['Go to the main menu'] 
+                }
+            ])
+                // add department here prompted to enter the name of the department and that department is added to the database
+            .then(function (selectedAnswer) {
+                if (selectedAnswer.ChoosingOptions === "Go to the main menu") {
+                    init();
+
+
+                };
+            })
+
+
+        };
+
+
+
     })
-    
-
-  };
-
-    
-
- })
 }
 
 init();
@@ -98,10 +106,10 @@ init();
 // THEN I am presented with a formatted table showing department names and department ids  *
 // WHEN I choose to view all roles *
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role *
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to view all employees *
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to *
+// WHEN I choose to add a department *
+// THEN I am prompted to enter the name of the department and that department is added to the database *
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 // WHEN I choose to add an employee
