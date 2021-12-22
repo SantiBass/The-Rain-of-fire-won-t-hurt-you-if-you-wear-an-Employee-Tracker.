81ab68234638d8d0a56a10fb6fa0f9cbae736187
SantiBass <sibarraryan@gmail.com>
2021-12-21 16:53:32 -0800

    working on the view all roles  options

--- server.js
+++ server.js
@@ -13,39 +13,84 @@ message: "===============================================\n  ****  Welcome to th
 choices: ['View all departments','View all roles', 'View all employees', 'Add departments', 'Add a role', 'Add an employee', 'Update  employee role',  'quit']
 }).then(function(selectedAnswer){
     if(selectedAnswer.ChoosingOptions === "View all departments"){
-        inquirer
-        .prompt({
+        inquirer.prompt({
             // add table HERE for departments BY NAME AND ID
             type: 'list',
             name: 'ChoosingOptions',
-            message: "Here are all departments \n  What would you like to do?",
+            message: "Here are all departments \n  Press enter to go to the main menu plase",
+            choices:['Go to the main menu']
             
-            choices:['Add a Department','Remove a department', 'Go to the main menu']
         }).then(function(selectedAnswer){
-            if(selectedAnswer.ChoosingOptions === "Add a Department"){
+            if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
+               init();
+
                 
-                
-            }else if(selectedAnswer.ChoosingOptions = "Go to the main menu"){
-                init();
-            };
+            }
         })
 
+     } else if(selectedAnswer.ChoosingOptions === "View all roles"){
+        inquirer.prompt({
+        // add table HERE for all roles presented with the job title, role id, the department that role belongs to, and the salary for that role
+        type: 'list',
+        name: 'ChoosingOptions',
+        message: "Here are all the roles \n  Press enter to go to the main menu plase",
+        choices:['Go to the main menu']
+    }).then(function(selectedAnswer){
+        if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
+           init();
+
+            
+        }
+    })
+
+ } else if(selectedAnswer.ChoosingOptions === "View all employees"){
+    inquirer.prompt({
+    // add table HERE for all employees presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    type: 'list',
+    name: 'ChoosingOptions',
+    message: "Here are all the employees \n  Press enter to go to the main menu plase",
+    choices:['Go to the main menu']
+}).then(function(selectedAnswer){
+    if(selectedAnswer.ChoosingOptions === "Go to the main menu"){
+       init();
+
+        
     }
+})
+
+ }
+
+    
 
 })
 }
 
 init();
 
+// inquirer.prompt([
+//     {
+//     type: 'input',
+//     name: 'nameOfDepartment',
+//     message: "Give a name to the department you want to add",
+    
+// },
+// {
+//     type: 'input',
+//     name: 'idOfDepartment',
+//     message: "Give an ID to the department you want to add",
+// }// add department here
+// ]).then(function(selectedAnswer){
+//     init();
+// })
 
 
 // GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I start the application *
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role *
+// WHEN I choose to view all departments *
+// THEN I am presented with a formatted table showing department names and department ids  *
+// WHEN I choose to view all roles *
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role *
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 // WHEN I choose to add a department
