0750ac12a2d6708f62e89dc03d308dcd884f87a2
SantiBass <sibarraryan@gmail.com>
2021-12-22 14:14:12 -0800

    working on the seed data file

--- db/query_db.sql
+++ db/query_db.sql
@@ -1,4 +1,6 @@
-SELECT * FROM departments;
+SELECT * FROM departments
 
 -- selecting data from from all roles.
-SELECT 
+SELECT  r.id, title, salary, department_name AS department
+FROM roles role_id
+JOIN departments  ON  d.department_id = d.id;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO  departments (id, username)
+VALUES
+       (1, "DOG"),
+       (2, "SDKJFHKJ"),
+       (3, "ASD;LKFASDLK"), 
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES
+        (1, )
+        ()
+        ()
+INSERT INTO 
