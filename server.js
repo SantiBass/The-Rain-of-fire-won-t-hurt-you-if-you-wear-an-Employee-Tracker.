02ad59d512874ed3764ee29d869fc3f644666c09
SantiBass <sibarraryan@gmail.com>
2021-12-21 16:02:54 -0800

    working on the view departments options

--- server.js
+++ server.js
@@ -7,21 +7,52 @@ require ("console.table");
 function init(){
 inquirer.prompt({
 type: "list",
-name : "ChosingOptions",
-message: "===============================================\n  ****  Welcome to the Employee Traker app.  ****\n  ****  What would you like to do today?     **** \n  ===============================================" ,
+name : "ChoosingOptions",
+message: "===============================================\n  ****  Welcome to the Employee Traker app.  ****\n  ****  What would you like to do today?     **** \n  =============================================== \n",
 
-choices: ['Add', 'remove', 'view', 'quit']
+choices: ['View all departments','View all roles', 'View all employees', 'Add departments', 'Add a role', 'Add an employee', 'Update  employee role',  'quit']
 }).then(function(selectedAnswer){
-    if(selectedAnswer.ChosingOptions === add){
-        inquirer.prompt({
+    if(selectedAnswer.ChoosingOptions === "View all departments"){
+        inquirer
+        .prompt({
+            // add table HERE for departments BY NAME AND ID
             type: 'list',
-            name: 'ChosingOptions',
-            message: "sdfgsdfg",
-            choices:['asdf','asdf','asdf','sdaf']
-        });
+            name: 'ChoosingOptions',
+            message: "Here are all departments \n  What would you like to do?",
+            
+            choices:['Add a Department','Remove a department', 'Go to the main menu']
+        }).then(function(selectedAnswer){
+            if(selectedAnswer.ChoosingOptions === "Add a Department"){
+                
+                
+            }else if(selectedAnswer.ChoosingOptions = "Go to the main menu"){
+                init();
+            };
+        })
 
-    };
+    }
+
 })
 }
 
 init();
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
